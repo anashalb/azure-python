36864cd96c333a1c242bece6685d7690ea5f4b14
Anas Halbawi <anashalb@gmail.com>
2018-03-15 09:01:59 +0400

    Only commit when necessary.. Duh\!

--- push.sh
+++ push.sh
@@ -3,19 +3,25 @@
 set -ex
 
 # Fetch the Python version from the Dockerfile VERSION environment variable
-VERSION=$(grep -E '^ENV VERSION' Dockerfile | grep -o -E '[0-9]+(\.[0-9]+)*')
+VERSION=$(awk '$2 == "VERSION" { print $3; exit }' Dockerfile)
 
 # Fetch the Python version from the Dockerfile PYTHON_VERSION environment variable
-PYVERSION=$(grep -E '^ENV PYTHON_VERSION' Dockerfile | grep -o -E '[0-9]+(\.[0-9]+)*')
+PYVERSION=$(awk '$2 == "PYTHON_VERSION" { print $3; exit }' Dockerfile)
 
 IMAGE=azure-python:$PYVERSION-$VERSION
 
-git add .
+if git diff-index --quiet HEAD --; then
+    echo "No changes were made to the repository. Pushing suspended."
+else
+    
+	git add .
 
-git commit -m "Automated Build Python Version $PYVERSION - Image Version $VERSION"
+	git commit -m "Automated Build Python Version $PYVERSION - Image Version $VERSION"
+
+fi
 
 git pull origin
 
 git tag -a "$PYVERSION-$VERSION" -m "Automated build version $PYVERSION-$VERSION"
 
-git push origin
+git push origin	
